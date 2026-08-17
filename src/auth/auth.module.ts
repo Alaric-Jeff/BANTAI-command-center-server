f266a8ad9f8a057ad6eba1e1e789dd221e6a5aba
Jeffrey M. Aspiras <jefsohandsome1@gmail.com>
2026-08-18 02:11:38 +0800

    stuff

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { AuthRepository } from './auth.repository';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { DatabaseModule } from '../database/database.module';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({}),
+    ConfigModule,
+    DatabaseModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, AuthRepository, JwtStrategy],
+  exports: [AuthService, JwtStrategy, PassportModule],
+})
+export class AuthModule {}
